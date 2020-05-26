db49eb0f1a6698a91bcc0fd039eeb3bcd331dd26
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-05-26 14:33:37 +0530

    [backend] added mongodb

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+    "mongodb+srv://device:device@cluster-7wx2i.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
