9dc65aa0d2dbe3d1b83c05192807f87ea78d67ef
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-01 23:41:36 +0530

    renamed frontend to lient

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ const usersRouter = require("./routes/user");
 app.use("/user", usersRouter);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
+    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
