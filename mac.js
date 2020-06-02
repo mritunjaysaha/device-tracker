95806aab2690d66e147b857d530e63a3dd29ac42
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-02 20:16:37 +0530

    replaced getmac with ipify

--- mac.js
+++ mac.js
@@ -1,10 +1,18 @@
-const getmac = require("getmac");
+// const getmac = require("getmac");
 
-const callMac = function () {
-    return getmac.default();
-};
+// const callMac = function () {
+//     return getmac.default();
+// };
 
-// const mac = callMac();
+// // const mac = callMac();
 
-// console.log(mac);
-console.log(callMac);
+// // console.log(mac);
+// console.log(callMac);
+
+var http = require("http");
+
+http.get({ host: "api.ipify.org", port: 80, path: "/" }, function (resp) {
+    resp.on("data", function (ip) {
+        console.log("My public IP address is: " + ip);
+    });
+});
