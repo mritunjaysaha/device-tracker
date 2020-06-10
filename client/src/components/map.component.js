04125bf61613b780de9be7143656fb60bc05d117
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-10 23:40:12 +0530

    map using react hooks

--- client/src/components/map.component.js
+++ client/src/components/map.component.js
@@ -1,45 +1,43 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React, { useState, useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken =
-    "pk.eyJ1IjoibXJpdHVuamF5c2FoYSIsImEiOiJja2I3eXY3N20wYWxsMzFwZ2F4cXY0MmJvIn0.pQKIGfiOkHXQMhyKkCyPHQ";
+// mapboxgl.accessToken =
+//     "pk.eyJ1IjoibXJpdHVuamF5c2FoYSIsImEiOiJja2I3eXY3N20wYWxsMzFwZ2F4cXY0MmJvIn0.pQKIGfiOkHXQMhyKkCyPHQ";
 
-export default class MapBox extends React.Component {
-    constructor(props) {
-        super(props);
+const styles = {
+    width: "100vw",
+    height: "calc(100vh - 80px)",
+    position: "absolute",
+};
 
-        this.state = {
-            lat: 26.138125199999998,
-            lng: 91.7387746,
-            zoom: 14,
-        };
-    }
-    componentDidMount() {
-        const map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: "mapbox://styles/mapbox/streets-v11",
-            center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom,
-        });
-        map.on("move", () => {
-            this.setState({
-                lng: map.getCenter().lng.toFixed(4),
-                lat: map.getCenter().lat.toFixed(4),
-                zoom: map.getZoom().toFixed(2),
+const MapboxGLMap = () => {
+    const [map, setMap] = useState(null);
+    const mapContainer = useRef(null);
+
+    useEffect(() => {
+        mapboxgl.accessToken =
+            "pk.eyJ1IjoibXJpdHVuamF5c2FoYSIsImEiOiJja2I3eXY3N20wYWxsMzFwZ2F4cXY0MmJvIn0.pQKIGfiOkHXQMhyKkCyPHQ";
+        const initializeMap = ({ setMap, mapContainer }) => {
+            const map = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+                center: [91.7387746, 26.138125199999998],
+                zoom: 14,
             });
-        });
-        var marker = new mapboxgl.Marker()
-            .setLngLat([91.7387746, 26.138125199999998])
-            .addTo(map);
-    }
 
-    render() {
-        return (
-            <div
-                ref={(el) => (this.mapContainer = el)}
-                className="mapContainer"
-            ></div>
-        );
-    }
-}
+            map.on("load", () => {
+                setMap(map);
+                map.resize();
+            });
+            var marker = new mapboxgl.Marker()
+                .setLngLat([91.7387746, 26.138125199999998])
+                .addTo(map);
+        };
+
+        if (!map) initializeMap({ setMap, mapContainer });
+    }, [map]);
+
+    return <div ref={(el) => (mapContainer.current = el)} style={styles} />;
+};
+
+export default MapboxGLMap;
