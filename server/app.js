bcafe7302d2fb3cc313f994e2f96af4a3b681a68
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-10-23 13:26:33 +0530

    added user model

---
+++ server/app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Database connection
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB connection established");
+});
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// My Routes
+app.get("/", (req, res) => res.send("Server is up and running"));
+
+// PORT
+const PORT = process.env.PORT || 8000;
+
+// Starting a server
+
+app.listen(PORT, () => {
+    console.log(`app is running at http://localhost:${PORT}`);
+});
